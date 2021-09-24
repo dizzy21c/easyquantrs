a09abd65b5ad3b1530c58ab3a0fcd202f0853876
dizzy21c <dizzy21c@gmail.com>
2021-09-24 17:42:07 +0800

    code backup

---
+++ livedata/examples/async_ex02.rs
@@ -0,0 +1,11 @@
+use futures::executor::block_on;
+
+async fn learn_song() -> Song { /* ... */ }
+async fn sing_song(song: Song) { /* ... */ }
+async fn dance() { /* ... */ }
+
+fn main() {
+    let song = block_on(learn_song());
+    block_on(sing_song(song));
+    block_on(dance());
+}
